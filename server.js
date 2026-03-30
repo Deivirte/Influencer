2be07d6d9b8c89df3bc0d55cf3b3f15b517be1fb
Deivirte <deivirte@gmail.com>
2026-03-30 15:13:14 -0300

    fix arrow function listen

--- server.js
+++ server.js
@@ -33,6 +33,6 @@ app.use("/api/upload", uploadRoutes);
 
 const PORT = process.env.PORT || 10000;
 
-app.listen(PORT, '0.0.0.0', () {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
